4146942fa4d30ac9fec9a32e4cf69e0c53810e18
Devcon4 <devcon42@gmail.com>
2018-05-01 23:25:03 -0600

    Init and start terminal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TerminalComponent } from './terminal/terminal.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TerminalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/terminal/terminal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terminal',
+  templateUrl: './terminal.component.html',
+  styleUrls: ['./terminal.component.scss']
+})
+export class TerminalComponent implements OnInit {
+
+  lines: string[] = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
